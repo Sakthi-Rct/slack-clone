b4383e333357526d157cddc28df7ca116cbb8c4b
sakthivel-happyfox <sakthivel@happyfox.com>
2021-03-13 00:52:16 +0530

    Slack clone completed

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,17 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../Chat.css";
 import { useParams } from "react-router-dom";
 import StarBorderOutlinedIcon from "@material-ui/icons/StarBorderOutlined";
 import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
+import db from "../firebase";
+import Message from "./Message";
+import ChatInput from "./ChatInput";
 
 function Chat() {
   const { roomId } = useParams();
+  const [roomDetails, setRoomDetails] = useState(null);
+  const [roomMessages, setRoomMessages] = useState([]);
+
+  const getMessages = () => {
+    db.collection("rooms")
+      .doc(roomId)
+      .collection("messages")
+      .orderBy("timestamp", "asc")
+      .onSnapshot((snapshot) => {
+        let messages = snapshot.docs.map((doc) => doc.data());
+        setRoomMessages(messages);
+      });
+  };
+
+  const getChannel = () => {
+    db.collection("rooms")
+      .doc(roomId)
+      .onSnapshot((snapshot) => setRoomDetails(snapshot.data()));
+  };
+
+  useEffect(() => {
+    getChannel();
+    getMessages();
+  }, [roomId]);
+
   return (
     <div className="chat">
       <div className="chat__header">
         <div className="chat__headerLeft">
           <h4 className="chat__channelName">
-            <strong>#general</strong>
+            <strong>#{roomDetails?.name}</strong>
             <StarBorderOutlinedIcon />
           </h4>
         </div>
@@ -22,6 +50,19 @@ function Chat() {
           </p>
         </div>
       </div>
+
+      <div className="chat__messgaes">
+        {roomMessages.map(({ message, timestamp, user, userImage }) => (
+          <Message
+            message={message}
+            timestamp={timestamp}
+            user={user}
+            userImage={userImage}
+          />
+        ))}
+      </div>
+
+      <ChatInput channelName={roomDetails?.name} channelId={roomId} />
     </div>
   );
 }
